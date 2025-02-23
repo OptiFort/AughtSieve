89634fa770d67388deb377d072690aeaa6545a15
binary number <57023457+binary-number@users.noreply.github.com>
2025-02-23 14:31:52 +0900

    [add] project

---
+++ AughtSieve.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AughtSieve.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
