40fe1efa726e9225cbfe5367adc518a480f69d40
425ta <425ta@DYNABOOK>
2025-02-23 16:11:13 +0900

    login画面作成

--- AughtSieve.Desktop/MainWindow.axaml.cs
+++ AughtSieve.Desktop/MainWindow.axaml.cs
@@ -7,6 +7,20 @@ namespace AughtSieve.Desktop
         public MainWindow()
         {
             InitializeComponent();
+            Button1.Click += Button1_Click;
+
+            
+
+        }
+
+        string user,passward;
+
+        private void Button1_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            user = TextBox1.Text;
+            passward= TextBox2.Text;
+
+
         }
     }
 }
